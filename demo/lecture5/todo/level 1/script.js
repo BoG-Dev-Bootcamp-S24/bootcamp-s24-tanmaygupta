5ad64edef1ae523dcb41501476a40690701a78eb
Tanmay102 <tanmaygupta102@gmail.com>
2024-02-08 19:50:09 -0500

    Finish level 1 of ToDo App (add button functionality)

--- demo/lecture5/todo/level 1/script.js
+++ demo/lecture5/todo/level 1/script.js
@@ -1,2 +1,16 @@
 //solution
 
+document.getElementById('add-button').addEventListener('click', function() {
+    let toDoList = document.getElementById('todo-list');
+    let toDoInput = document.getElementById('todo-input');
+    let newTodo = toDoInput.value;
+
+    if (newTodo.trim() !== "") {
+        let listItem = document.createElement('li');
+        listItem.textContent = newTodo;
+        toDoList.appendChild(listItem);
+        toDoInput.value = "";
+    } else {
+        alert("Please enter a task before trying to add it");
+    }
+})
