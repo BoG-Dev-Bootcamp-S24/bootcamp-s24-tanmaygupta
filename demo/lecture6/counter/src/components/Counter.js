7e70e3d6bb237cf560a120efec7c2dd355c05578
Tanmay102 <tanmaygupta102@gmail.com>
2024-02-13 19:58:46 -0500

    Finished lecture6 demo counter

--- demo/lecture6/counter/src/components/Counter.js
+++ demo/lecture6/counter/src/components/Counter.js
@@ -6,17 +6,24 @@ import ResetButton from './reset/ResetButton';
 
 
 function Counter() {
-    let count = 0;
+
+    const [count, setCount] = useState(0);
   
     const increment = () => {
+      setCount(count + 1);
     };
   
     const decrement = () => {
+      setCount(count - 1);
       }
   
     return (
       <div className="counter">
-        
+        <h1> Counter </h1>
+        {count}
+        <br/>
+        <button onClick = {increment} class = "counter button"> Increment </button>
+        <button onClick = {decrement} class = "counter button"> Decrement </button>
       </div>
     );
   }
