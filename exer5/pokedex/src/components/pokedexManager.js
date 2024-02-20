a3d14919bc13310acbd86af143688fbe38033cf3
Tanmay102 <tanmaygupta102@gmail.com>
2024-02-20 02:04:28 -0500

    Create components folder and PokedexManager component

---
+++ exer5/pokedex/src/components/pokedexManager.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+const PokedexManager = (() => {
+
+    return (
+        <div className="pokedex-manager">
+            <h1> This the Pokedex Manager </h1>
+        </div>
+    )
+})
+
+export default PokedexManager;
