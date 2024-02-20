9f39a979c04c71885e6d8ae42b1ad73d391f6062
Tanmay102 <tanmaygupta102@gmail.com>
2024-02-20 15:54:50 -0500

    Update right and left navigation button styling

--- exer5/pokedex/src/components/PokeDexLeft.js
+++ exer5/pokedex/src/components/PokeDexLeft.js
@@ -5,6 +5,8 @@ const PokeDexLeft = (({pokemonName, pokemonSprite, pokemonTypes, modifyIndex}) =
     const sprite = pokemonSprite;
     const types = pokemonTypes;
 
+    const leftArrow = "https://s3-alpha-sig.figma.com/img/a29b/af48/758326061f2ccf982c8f4dcfbd0ef5cf?Expires=1709510400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=D-s27cf1YjApqWpRiPym8lfbJDkNdRxT-Ln7RqBK0opbZ7ce9zhza4MDupPm1vEi0zHxxiRsN8lCavo2KG19ZkGNzNOxKJ2mVrXGiziGNu33DOZFsxDNIQ1GBNND0NdZF8rQf9Uj2TfFjF1xHwoVOHRQeWEhVsd6TQ-2tuXwBtkWfTKqMiRRXYlNgIcnjR7LEdqvekpEGXZRBqCAWVlmXytsjg9kUe4FTmKyRTf-dtgbesI~bO47BB-KNWnQTzx-cu06rjJDEk7dyONDtjZNm8Pt6HCcW47hX49i9OFpxbtL4ShDeuR3kIGSDOA4tsUT779kovUJtTAkWbMD5bSILA__"
+
     const increment = (() => modifyIndex(1))
 
     const decrement = (() => modifyIndex(-1))
@@ -25,8 +27,8 @@ const PokeDexLeft = (({pokemonName, pokemonSprite, pokemonTypes, modifyIndex}) =
                 })}
             </div>
             <div className="buttons">
-                <button className="button" onClick={decrement}> &#60; </button>
-                <button className="button" onClick={increment}> &#62; </button>
+                <button className="button" onClick={decrement}> <img style={{border: 'none', width: '30%'}} src={leftArrow} alt="Left arrow" /> </button>
+                <button className="button" onClick={increment}> <img style={{border: 'none', width: '30%', transform: 'scaleX(-1)'}} src={leftArrow} alt="Right arrow" />  </button>
             </div>
         </div>
     )
