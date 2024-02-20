b38a20b765cb7fc9622cfb587eb4883e297028ed
Tanmay102 <tanmaygupta102@gmail.com>
2024-02-20 16:36:51 -0500

    Basic logic for switching between info and moves tabs

--- exer5/pokedex/src/components/PokeDexRight.js
+++ exer5/pokedex/src/components/PokeDexRight.js
@@ -1,9 +1,37 @@
 import "./PokeDexRight.css"
+import { useState } from "react";
 
 const PokeDexRight = (() => {
+
+    const [choice, setChoice] = useState(0);
+
+    const moves = (() => {
+        setChoice(1);
+    })
+
+    const info = (() => {
+        setChoice(0);
+    })
+
+    const displayInfo = (() => {
+        return (
+            <h1> Info selected </h1>
+        )
+    })
+
+    const displayMoves = (() => {
+        return (
+            <h1> Moves selected </h1>
+        )
+    })
+
     return (
         <div className="pokedex-right">
-            <h1>Right Side</h1>
+            { choice === 0 ? displayInfo() : displayMoves() }
+            <div className="buttons">
+                <button onClick={info}> Info </button>
+                <button onClick={moves}> Moves </button>
+            </div>
         </div>
     )
 })
